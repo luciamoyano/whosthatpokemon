b9509071499722b41ab6a0529667f213388e8021
Pedro <pkupersmit@gmail.com>
2021-03-21 23:33:25 -0300

    Home actualizado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Button from "../../components/Button";
 import styles from "./Home.module.scss";
 
 function Home() {
   return (
     <div className={styles.container}>
-      <Button difficulty="easy" />
-      <Button difficulty="medium" />
-      <Button difficulty="hard" />
+      <h1>Bienvenido!</h1>
+      <Link className={styles.link} to="/challenge">
+        Hacé click acá para jugar
+      </Link>
     </div>
+    // <div className={styles.container}>
+    //   <Button difficulty="easy" />
+    //   <Button difficulty="medium" />
+    //   <Button difficulty="hard" />
+    // </div>
   );
 }
 
